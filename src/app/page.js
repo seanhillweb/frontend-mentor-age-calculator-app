5a4c97f701e67b7f4349e7418caf11fd2b40b26e
seanhillweb <seanhillweb@gmail.com>
2023-08-08 10:43:33 -0500

    feat: add age calculator component

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,206 @@
-import Image from 'next/image'
+"use client";
+
+/**
+ * Summary. Frontend Mentor Challenge.
+ *
+ * Description. An age calculator app.
+ *
+ * @link https://www.freecodecamp.org/news/how-to-build-forms-in-react/
+ */
+
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+  const date = new Date();
+
+  const currentDay = date.getDate();
+  const currentMonth = date.getMonth() + 1;
+  const currentYear = date.getFullYear();
+
+  const currentFullDate = `${currentDay}-${currentMonth}-${currentYear}`;
+
+  console.log(currentFullDate);
+
+  const [isDay, setDay] = useState(currentDay);
+  const [isMonth, setMonth] = useState(currentMonth);
+  const [isYear, setYear] = useState(currentYear);
+
+  const [isSubmittedDate, setSubmittedDate] = useState();
+
+  function Form() {
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => {
+      console.log(data);
+      // setDay(data.day);
+      // setMonth(data.month);
+      // setYear(data.year);
+      // console.log(isDay, isMonth, isYear);
+    };
+
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid grid-cols-3 md:grid-cols-4 gap-4">
+          <div className="flex flex-col">
+            <label
+              htmlFor="day"
+              className={`text-xs font-bold tracking-[0.2em] uppercase mb-1 ${
+                errors.day ? "text-lightRed" : "text-smokeyGrey"
+              }`}
+            >
+              Day
+            </label>
+            <input
+              id="day"
+              type="text"
+              name="day"
+              className={`font-extrabold text-xl rounded-lg p-3 focus:ring-transparent hover:cursor-pointer ${
+                errors.day
+                  ? "text-lightRed border-lightRed hover:border-lightRed focus:border-lightRed"
+                  : "border-lightGrey hover:border-purple focus:border-purple"
+              }`}
+              placeholder="DD"
+              inputMode="numeric"
+              maxLength={2}
+              aria-invalid={errors.day ? "true" : "false"}
+              {...register("day", {
+                required: true,
+                pattern: "(0[1-9]|[12][0-9]|3[01])",
+                maxLength: 2,
+              })}
             />
-          </a>
+            {errors.day && errors.day.type === "required" && (
+              <span role="alert" className="text-lightRed text-xs italic mt-1">
+                This field is required
+              </span>
+            )}
+            {errors.day > 31 || errors.day < 1 && (
+              <span role="alert" className="text-lightRed text-xs italic mt-1">
+                Must be a valid day
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor="month"
+              className={`text-xs font-bold tracking-[0.2em] uppercase mb-1 ${
+                errors.month ? "text-lightRed" : "text-smokeyGrey"
+              }`}
+            >
+              Month
+            </label>
+            <input
+              id="month"
+              type="text"
+              name="month"
+              className={`font-extrabold text-xl rounded-lg px-4 py-3 focus:ring-transparent hover:cursor-pointer ${
+                errors.month
+                  ? "text-lightRed border-lightRed hover:border-lightRed focus:border-lightRed"
+                  : "border-lightGrey hover:border-purple focus:border-purple"
+              }`}
+              placeholder="MM"
+              maxLength={2}
+              aria-invalid={errors.month ? "true" : "false"}
+              {...register("month", {
+                required: true,
+                pattern: "(0[1-9]|1[1,2])",
+                maxLength: 2,
+                valueAsNumber: true,
+              })}
+            />
+            {errors.month && errors.month.type === "required" && (
+              <span role="alert" className="text-lightRed text-xs italic mt-1">
+                This field is required
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor="year"
+              className={`text-xs font-bold tracking-[0.2em] uppercase mb-1 ${
+                errors.year ? "text-lightRed" : "text-smokeyGrey"
+              }`}
+            >
+              Year
+            </label>
+            <input
+              id="year"
+              type="text"
+              name="year"
+              className={`font-extrabold text-xl rounded-lg px-4 py-3 focus:ring-transparent hover:cursor-pointer ${
+                errors.year
+                  ? "text-lightRed border-lightRed hover:border-lightRed focus:border-lightRed"
+                  : "border-lightGrey hover:border-purple focus:border-purple"
+              }`}
+              placeholder="YYYY"
+              minLength={4}
+              maxLength={4}
+              aria-invalid={errors.year ? "true" : "false"}
+              {...register("year", {
+                required: true,
+                pattern: "(19|20)d{2}",
+                minLength: 4,
+                maxLength: 4,
+                valueAsNumber: true,
+              })}
+            />
+            {errors.year && errors.year.type === "required" && (
+              <span role="alert" className="text-lightRed text-xs italic mt-1">
+                This field is required
+              </span>
+            )}
+          </div>
         </div>
-      </div>
+        <div className="relative my-16 md:my-20">
+          <hr className="border-t-2 border-lightGrey" aria-hidden={true} />
+          <button
+            type="submit"
+            className="absolute top-0 -translate-y-[50%] left-0 right-0 md:left-auto mx-auto w-16 md:w-20 h-16 md:h-20 rounded-full bg-purple hover:bg-offBlack focus:bg-offBlack transition duration-200 ease-in-out text-center"
+          >
+            <Image
+              src="/icon-arrow.svg"
+              alt="arrow icon"
+              width={46}
+              height={44}
+              aria-hidden={true}
+              className="inline"
+            />
+            <span className="sr-only">Calculate age</span>
+          </button>
+        </div>
+      </form>
+    );
+  }
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  return (
+    <div className="container">
+      <main
+        id="main"
+        className="flex flex-col items-center justify-center min-h-screen"
+        aria-label="Content"
+        aria-live="polite"
+      >
+        <div className="w-full md:max-w-[700px] bg-white p-6 md:p-12 rounded-3xl rounded-br-[4.5rem] md:rounded-br-[9rem]">
+          <Form />
+          <ul className="list-none font-extrabold italic text-5xl md:text-7xl">
+            <li>
+              <span className="text-purple">--</span> years
+            </li>
+            <li>
+              <span className="text-purple">--</span> months
+            </li>
+            <li>
+              <span className="text-purple">--</span> days
+            </li>
+          </ul>
+        </div>
+      </main>
+    </div>
+  );
 }
